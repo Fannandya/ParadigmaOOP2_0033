4f9dc9fe6c199b75f74bc5ac2d3321808c8a6e8b
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-06-02 10:13:37 +0700

    membuat function main dan membuat objek dari class

--- enkapsulasi/enkapsulasi.cpp
+++ enkapsulasi/enkapsulasi.cpp
@@ -16,3 +16,13 @@ public:
         return saklarNo[i];
     }
 };
+
+int main()
+{
+    remoteLampu lampurumah;
+
+    lampurumah.setSaklarNo(0, "lampu teras rumah");
+    lampurumah.setSaklarNo(1, "lampu ruang tamu");
+    lampurumah.setSaklarNo(2, "lampu kamar tidur");
+    lampurumah.setSaklarNo(3, "lampu tidur");
+}
